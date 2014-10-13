8964b4dcaafaba1c02a4e7bd85b09b583cff0179
Herr Ritschwumm <ritschwumm@djini.de>
2014-10-13 21:44:05 +0200

    updated libraries, adapted code, changed key names

--- src/main/scala/CapsulePlugin.scala
+++ src/main/scala/CapsulePlugin.scala
@@ -1,22 +1,29 @@
 package xsbtCapsule
 
+import java.util.jar.Attributes.Name._
+
 import sbt._
 import Keys.TaskStreams
 
-import xsbtUtil._
+import xsbtUtil.implicits._
+import xsbtUtil.types._
+import xsbtUtil.{ util => xu }
+
 import xsbtClasspath.{ Asset => ClasspathAsset, ClasspathPlugin }
 import xsbtClasspath.Import.classpathAssets
 
 object Import {
+	val capsuleBuildDir				= settingKey[File]("base directory of built files")
+	val capsulePackageName			= settingKey[String]("name of the package built")
+	
 	val capsule						= taskKey[File]("complete build, returns the created capsule jar")
-	val capsuleOutputDir			= taskKey[File]("where to put the capsule jar")
-	val capsuleJarName				= taskKey[String]("the name of the capsule jar")
 	val capsuleJarFile				= taskKey[File]("the capsule jar file")
+	
 	val capsuleMainClass			= taskKey[Option[String]]("name of the main class")
 	val capsuleVmOptions			= settingKey[Seq[String]]("vm options like -Xmx128")
 	val capsuleSystemProperties		= settingKey[Map[String,String]]("-D in the command line")
 	val capsuleMinJavaVersion		= settingKey[Option[String]]("minimum java version")
-	val capsulePrependExecHeader	= settingKey[Boolean]("include exec header")
+	val capsuleMakeExecutable		= settingKey[Boolean]("make the jar file executable on unixoid systems")
 }
 
 object CapsulePlugin extends AutoPlugin {
@@ -31,36 +38,38 @@ object CapsulePlugin extends AutoPlugin {
 	//------------------------------------------------------------------------------
 	//## exports
 	
-	override def requires:Plugins		= ClasspathPlugin
+	override val requires:Plugins		= ClasspathPlugin && plugins.JvmPlugin
 	
-	override def trigger:PluginTrigger	= allRequirements
+	override val trigger:PluginTrigger	= noTrigger
 
 	lazy val autoImport	= Import
 	import autoImport._
 	
-	override def projectSettings:Seq[Def.Setting[_]]	=
+	override lazy val projectSettings:Seq[Def.Setting[_]]	=
 			Vector(
+				capsuleBuildDir			:= Keys.crossTarget.value / "capsule",
+				
 				capsule		:=
 						buildTask(
 							streams				= Keys.streams.value,
 							assets				= classpathAssets.value,
 							jarFile				= capsuleJarFile.value,
-							applicationName		= Keys.version.value,
-							applicationVersion	= Keys.name.value,
+							applicationName		= Keys.name.value,
+							applicationVersion	= Keys.version.value,
 							applicationClass	= capsuleMainClass.value,			
 							vmOptions			= capsuleVmOptions.value,
 							systemProperties	= capsuleSystemProperties.value,
 							minJavaVersion		= capsuleMinJavaVersion.value,
-							prependExecHeader	= capsulePrependExecHeader.value
+							makeExecutable		= capsuleMakeExecutable.value
 						),
-				capsuleOutputDir			:= Keys.crossTarget.value / "capsule",
-				capsuleJarName				:= Keys.name.value + "-" + Keys.version.value + ".jar",
-				capsuleJarFile				:= capsuleOutputDir.value / capsuleJarName.value,
+				capsuleJarFile				:= capsuleBuildDir.value / (capsulePackageName.value + ".jar"),
+				
+				capsulePackageName			:= Keys.name.value + "-" + Keys.version.value,
 				capsuleMainClass			:= Keys.mainClass.value,
 				capsuleVmOptions			:= Seq.empty,
 				capsuleSystemProperties		:= Map.empty,
 				capsuleMinJavaVersion		:= None,
-				capsulePrependExecHeader	:= false
+				capsuleMakeExecutable		:= false
 			)
 	
 	//------------------------------------------------------------------------------
@@ -76,19 +85,21 @@ object CapsulePlugin extends AutoPlugin {
 		vmOptions:Seq[String],
 		systemProperties:Map[String,String],
 		minJavaVersion:Option[String],
-		prependExecHeader:Boolean
+		makeExecutable:Boolean
 	):File =
 			IO withTemporaryDirectory { tempDir =>
 				val applicationClassGot	=
-						applicationClass	getOrElse
-						failWithError(streams, s"${capsuleMainClass.key.label} must be set")
+						applicationClass	getOrElse {
+							xu.fail logging (streams, s"${capsuleMainClass.key.label} must be set")
+						}
 				val minJavaVersionGot	=
-						minJavaVersion		getOrElse
-						failWithError(streams, s"${capsuleMinJavaVersion.key.label} must be set")
+						minJavaVersion		getOrElse {
+							xu.fail logging (streams, s"${capsuleMinJavaVersion.key.label} must be set")
+						}
 						
 				val capsuleClassFile	= tempDir / capsuleClassName
 				IO download (
-					resourceURL(capsuleClassResource),
+					xu.classpath url capsuleClassResource,
 					capsuleClassFile
 				)
 				
@@ -97,34 +108,33 @@ object CapsulePlugin extends AutoPlugin {
 						(assets map { _.flatPathMapping })
 					
 				val manifest	= 
-						jarManifest(
-							// Attribute.Name.MANIFEST_VERSION
-							"Manifest-Version"		-> "1.0",
-							 // Attribute.Name.MAIN_CLASS
-							"Main-Class"			-> "Capsule",
-							"Application-Name"		-> applicationName,
-							"Application-Version"	-> applicationVersion,
-							"Application-Class"		-> applicationClassGot,
-							"System-Properties"		-> (systemProperties map { case (k, v) => k + "=" + v } mkString " "),
-							"JVM-Args"				-> (vmOptions mkString " "),
-							"Min-Java-Version"		-> minJavaVersionGot
+						xu.jar manifest (
+							MANIFEST_VERSION.toString	-> "1.0",
+							MAIN_CLASS.toString			-> "Capsule",
+							"Application-Name"			-> applicationName,
+							"Application-Version"		-> applicationVersion,
+							"Application-Class"			-> applicationClassGot,
+							"System-Properties"			-> (systemProperties map { case (k, v) => k + "=" + v } mkString " "),
+							"JVM-Args"					-> (vmOptions mkString " "),
+							"Min-Java-Version"			-> minJavaVersionGot
 						)
 						
 				streams.log info s"building capsule file ${jarFile}"
 				jarFile.mkParentDirs()
 				
-				if (prependExecHeader) {
-					val tempJar		= tempDir / "capsule.jar"
+				if (makeExecutable) {
+					val tempJar	= tempDir / "capsule.jar"
 					IO jar		(jarSources, tempJar, manifest)
 					
-					IO write	(jarFile, resourceBytes(execHeaderResource))
+					IO write	(jarFile, xu.classpath bytes execHeaderResource)
 					IO append	(jarFile, IO readBytes tempJar)
+					
+					jarFile setExecutable (true, false )
 				}
 				else {
 					IO jar (jarSources, jarFile, manifest)
 				}
 			
-				jarFile setExecutable (true, false)
 				jarFile
 			}
 }
